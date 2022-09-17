26ecffb784974d1f29f8469392e0c988ed972696
roughentomologyx <45817753+roughentomologyx@users.noreply.github.com>
2022-09-17 15:53:36 -0400

    Update NFTdraft.sol
    asd

--- NFTdraft.sol
+++ NFTdraft.sol
@@ -1,56 +1,75 @@
-// SPDX-License-Identifier: MIT
+  // SPDX-License-Identifier: MIT
 
-// hashlips erc1155 template used
+  // hashlips erc1155 template used
 
-pragma solidity ^0.8.0;
+  pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+  import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+  import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract NFT1155 is ERC1155, Ownable {
+  contract NFT1155 is ERC1155, Ownable {
 
-  string public name;
-  string public symbol;
-  address[] public receiverAddresses;
+    string public name;
+    string public symbol;
+    uint public startingTime;
+      uint public endTime;
+    address[] public receiverAddresses;
+    uint public totalSupply;
+    mapping(uint => string) public tokenURI;
+    address public evaluatorAddress;
 
-  mapping(uint => string) public tokenURI;
+    constructor(uint _endtime, address _evaluator, uint evalCounterIterator, uint evalCounterMax) ERC1155("") {
+      name = "Hypercerts";
+      symbol = "Hypercerts";
+      startingTime = block.timestamp;
+      endTime = _endtime;
+      evaluatorAddress = _evaluator;
+      evalCounterIterator=;
+      evalCounterMax=;
 
-  constructor(address[] memory _receiverAddresses) ERC1155("") {
-    name = "Hypercerts";
-    symbol = "Hypercerts";
-    receiverAddresses = _receiverAddresses;
+    }
+    // evalutor exchanges Hypercert
+    function updateCert(uint _id, string memory _uri) private {
+      require evalCounter <
+      require(msg.sender == evaluatorAddress, "you are not the evaluator");
+      tokenURI[_id] = _uri;
+      emit URI(_uri, _id);
+    }
+    function mint(address _to, uint _id, uint _amount) internal { //
+      er
+      _mint(_to, _id, _amount, "");
+      totalSupply += _amount;
+    }
+
+    function mintBatch(address _to, uint[] memory _ids, uint[] memory _amounts) external onlyOwner {
+      _mintBatch(_to, _ids, _amounts, "");
+    }
+
+    function burn(uint _id, uint _amount) external {
+      _burn(msg.sender, _id, _amount);
+    }
+
+    function burnBatch(uint[] memory _ids, uint[] memory _amounts) external {
+      _burnBatch(msg.sender, _ids, _amounts);
+    }
+    function burnForMint(address _from, uint[] memory _burnIds, uint[] memory _burnAmounts, uint[] memory _mintIds, uint[] memory _mintAmounts) external onlyOwner {
+      _burnBatch(_from, _burnIds, _burnAmounts);
+      _mintBatch(_from, _mintIds, _mintAmounts, "");
+    }
+    function percentageShare(address holder) public view returns (uint){
+      uint a = balanceOf(holder, 0);
+      uint b = totalSupply;
+      uint c = (100*a)/(b);
+      return c;
+    }
+    function uri(uint _id) public override view returns (string memory) {
+      return tokenURI[_id];
+    }
+    function buycert(uint _id) public payable {
+      require(msg.value > 10, "minimum funding not met");
+      require(block.timestamp < endTime, "Cert has already matured");
+      //uint fundingmass = msg.value;
+      mint( msg.sender,  _id, msg.value);
+    }
   }
-  // evalutor exchanges Hypercert
-  function setURI(uint _id, string memory _uri) private onlyOwner {
-    tokenURI[_id] = _uri;
-    emit URI(_uri, _id);
-  }
-  function mint(address _to, uint _id, uint _amount, string memory _uri) external onlyOwner { //
-    _mint(_to, _id, _amount, "");
-    setURI(_id, _uri);
-  }
-
-  function mintBatch(address _to, uint[] memory _ids, uint[] memory _amounts) external onlyOwner {
-    _mintBatch(_to, _ids, _amounts, "");
-  }
-
-  function burn(uint _id, uint _amount) external {
-    _burn(msg.sender, _id, _amount);
-  }
-
-  function burnBatch(uint[] memory _ids, uint[] memory _amounts) external {
-    _burnBatch(msg.sender, _ids, _amounts);
-  }
-  function burnForMint(address _from, uint[] memory _burnIds, uint[] memory _burnAmounts, uint[] memory _mintIds, uint[] memory _mintAmounts) external onlyOwner {
-    _burnBatch(_from, _burnIds, _burnAmounts);
-    _mintBatch(_from, _mintIds, _mintAmounts, "");
-  }
-
-  function uri(uint _id) public override view returns (string memory) {
-    return tokenURI[_id];
-  }
-  function buycert public payable {
-    int fundingmass = msg.value;
-  }
-}
 
